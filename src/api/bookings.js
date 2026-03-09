a37e8de8775a675cde0ab2de4042b13033b26089
JarlErikMathiesen <117684143+JarlErikMathiesen@users.noreply.github.com>
2026-03-09 12:50:39 +0100

    createBooking in api folder

---
+++ src/api/bookings.js
@@ -0,0 +1,8 @@
+export const createBooking = async (bookingData) => {
+  const data = await apiFetch('/holidaze/bookings', {
+    method: 'POST',
+    body: JSON.stringify(bookingData),
+  });
+
+  return data.data;
+};
